78a610301f4072a70819e74ba9bbe2b6b33ede23
Ronan <85257187+ronan-s1@users.noreply.github.com>
2022-01-07 23:09:42 +0000

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -32,20 +32,20 @@ CREATE TABLE reserved(
 );
 
 INSERT INTO books VALUES
-('093-403992', 'Computers in Business', 'Alicia Oneill', 3, 1997, 3, 'N'),
-('23472-8729', 'Exploring Peru', 'Stephanie Birchin', 4, 2005, 5, 'N'),
-('237-34823', 'Business Strategy', 'Joe Peppard', 2, 2002, 2, 'N'),
-('23u8-923849', 'A guide to nutrition', 'John Thorpe', 2, 1997, 1, 'N'),
-('82n8-308', 'computers for idiots', 'Susan O\'Neill', 5, 1998, 4, 'N'),
-('2983-3494', 'Cooking for children', 'Anabelle Sharper', 1, 2003, 7, 'N'),
-('9823-23984', 'My life in picture', 'Kevin Graham', 8, 2004, 1, 'N'),
-('9823-2403-0', 'Davinvi Code', 'Dan Brown', 1, 2003, 8, 'N'),
-('98234-029384', 'My rand in Texas', 'George Bush', 1, 2005, 1, 'N'),
-('9823-98345', 'How to cook Italian food', 'Jamie Oliver', 2, 2005, 7, 'N'),
-('9823-98487', 'Optimising your business', 'Cleo Blair', 1, 2001, 2, 'N'),
-('988745-234', 'Tara Toad', 'Maeve Binchy', 4, 2002, 8, 'N'),
-('993-004-00', 'My life in bits', 'John Smith', 1, 2001, 1, 'N'),
-('9987-0039882', 'Shooting History', 'Jon Snow', 1, 2003, 1, 'N');
+('093-403992', 'Computers in Business', 'Alicia Oneill', 3, 1997, '003', 'N'),
+('23472-8729', 'Exploring Peru', 'Stephanie Birchin', 4, 2005, '005', 'N'),
+('237-34823', 'Business Strategy', 'Joe Peppard', 2, 2002, '002', 'N'),
+('23u8-923849', 'A guide to nutrition', 'John Thorpe', 2, 1997, '001', 'N'),
+('82n8-308', 'computers for idiots', 'Susan O\'Neill', 5, 1998, '004', 'N'),
+('2983-3494', 'Cooking for children', 'Anabelle Sharper', 1, 2003, '007', 'N'),
+('9823-23984', 'My life in picture', 'Kevin Graham', 8, 2004, '001', 'N'),
+('9823-2403-0', 'Davinvi Code', 'Dan Brown', 1, 2003, '008', 'N'),
+('98234-029384', 'My rand in Texas', 'George Bush', 1, 2005, '001', 'N'),
+('9823-98345', 'How to cook Italian food', 'Jamie Oliver', 2, 2005, '007', 'N'),
+('9823-98487', 'Optimising your business', 'Cleo Blair', 1, 2001, '002', 'N'),
+('988745-234', 'Tara Toad', 'Maeve Binchy', 4, 2002, '008', 'N'),
+('993-004-00', 'My life in bits', 'John Smith', 1, 2001, '001', 'N'),
+('9987-0039882', 'Shooting History', 'Jon Snow', 1, 2003, '001', 'N');
 
 INSERT INTO categories VALUES
 ('001', 'Health'),
